29d1a7904e9ca44109af2f1e8fda6f01257692fd
seyed roohollah marashi <srmarashi@yahoo.com>
2018-04-19 02:00:56 -0700

    Add files via upload

---
+++ dac08088.ino
@@ -0,0 +1,23 @@
+
+
+#include <ShiftRegister74HC595.h>
+ShiftRegister74HC595 sr (1, 3, 4, 5);
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  
+  uint8_t   pinValues[] = { B11111111 };
+  sr.setAll(pinValues);
+  delay(10000);
+  sr.updateRegisters(); 
+
+
+
+  pinValues[0] = { B00000000 };
+  sr.setAll(pinValues);
+  delay(10000);
+  sr.updateRegisters(); 
+}
